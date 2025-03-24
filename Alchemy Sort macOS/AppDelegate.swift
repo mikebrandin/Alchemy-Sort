fa51262f1e211bbaea2ec4b72d87612dc2f47f6d
Mike Brandin <mikejbrandin@gmail.com>
2025-03-24 12:48:41 -0400

    Initial Commit

---
+++ Alchemy Sort macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Alchemy Sort macOS
+//
+//  Created by Mike Brandin on 3/24/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
